f4f0adece9d7f5e3933d4edcdb486797008ecf96
Vl-VSV <vvs_2015@icloud.com>
2022-10-10 12:58:32 +0300

    Add recognition function

--- FlowerClassifier/ViewController.swift
+++ FlowerClassifier/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
@@ -30,11 +32,37 @@ class ViewController: UIViewController {
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            if let clImage = CIImage(image: userPickedImage) {
+                detect(flowerImage: clImage)
+            }
             imageView.image = userPickedImage
         }
         
         imagePicker.dismiss(animated: true)
     }
     
+    
+    func detect(flowerImage image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to posses image.")
+            }
+            print(results)
+            self.title = results.first?.identifier
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
