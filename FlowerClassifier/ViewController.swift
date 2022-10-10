250ba1efbd8da59d833ec9d412ac10191cf35f3b
Vl-VSV <vvs_2015@icloud.com>
2022-10-10 19:10:52 +0300

    Add description

--- FlowerClassifier/ViewController.swift
+++ FlowerClassifier/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController()
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    
+    var parameters : [String:String] = [
+        "format" : "json",
+        "action" : "query",
+        "prop" : "extracts",
+        "exintro" : "",
+        "explaintext" : "",
+        "titles" : "flowerName",
+        "indexpageids" : "",
+        "redirects" : "1",
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +38,8 @@ class ViewController: UIViewController {
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = true
     }
-
-
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true)
     }
@@ -54,8 +70,13 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             guard let results = request.results as? [VNClassificationObservation] else {
                 fatalError("Model failed to posses image.")
             }
-            print(results)
-            self.title = results.first?.identifier
+            //print(results)
+            if let saveResultF = results.first {
+                self.title = saveResultF.identifier
+                self.parameters["titles"] = saveResultF.identifier
+                self.performRequest(flowerName: saveResultF.identifier)
+            }
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
@@ -63,6 +84,22 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             try handler.perform([request])
         } catch {
             print(error)
+        }
+    }
+    
+    func performRequest(flowerName: String) {
+        AF.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { response in
+            
+            switch response.result{
+            case .success(let value):
+                let json = JSON(value)
+                let pageid = json["query"]["pageids"][0].stringValue
+                self.descriptionLabel.text = json["query"]["pages"][pageid]["extract"].stringValue
+            
+            case .failure(let error):
+                print(error)
+            }
+            
         }
     }
 }
